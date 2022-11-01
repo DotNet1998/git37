04dc03b315c733cb268c31f01ecd5fc3019ec6d8
mamaand007@icloud.com <mamaand007@icloud.com>
2022-11-01 14:14:16 +0100

    Добавьте файлы проекта.

---
+++ git37/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace git37
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите число: #");
+            int x = int.Parse(Console.ReadLine());
+            Console.Write("Введите степень: #");
+            int y = int.Parse(Console.ReadLine());
+            stepen(x, y);
+            Console.WriteLine("Результат : " + stepen(x, y));
+
+        }
+        public static int stepen(int x, int y)
+        {
+            int res = 1;
+            for (int i = 0; i < y; i++)
+            {
+                res *= x;
+            }
+            return res;
+        }
+    }
+}
+
